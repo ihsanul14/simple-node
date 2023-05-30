f86c3df08c8fe38091c1f6c2254d08e955b6ee0f
Ihsanul Qolby <ihsanboga6415@gmail.com>
2023-05-30 14:20:09 +0700

    ci: add dockerfile

--- mocha.config.cjs
+++ mocha.config.cjs
@@ -1,13 +1,9 @@
-// require('ts-node').register({
-//     project: 'tsconfig.json'
-// });
-  
-//   module.exports = {
-//     extension: ['ts'
-//   ],
-//     recursive: true,
-//     timeout: 5000,
-//     reporter: 'spec',
-//     require: 'ts-node/register',
-// };
+module.exports = {
+    extension: ['ts'
+  ],
+    recursive: true,
+    timeout: 5000,
+    reporter: 'spec',
+    require: 'ts-node/register',
+};
   
