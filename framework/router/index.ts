20819b655c74231ba40aefe8fce429f89ee99547
Ihsanul Qolby <ihsanboga6415@gmail.com>
2024-02-20 09:54:58 +0700

    feat(apm): add span

--- framework/router/index.ts
+++ framework/router/index.ts
@@ -4,17 +4,21 @@ import { SwaggerUi, swaggerSpec } from '../swagger/index'
 import express,{ Request, Response, NextFunction } from 'express'
 import env from "../env/index";
 import APM from "../apm/index";
-
+import apm from "elastic-apm-node";
 
 export const apmAgent = new APM()
-
 export let router:any = express();
 class Router{
   Init(){
     router.use(express.json())
     router.use('/swagger', SwaggerUi.serve, SwaggerUi.setup(swaggerSpec));
     const apmTransaction = apmAgent.Start()
-    router.use((req:Request, res:Response, next:NextFunction) => {
+    router.use(this.ApmMiddleware(apmTransaction));
+    DataRoutes(router)  
+    return router
+  }
+  ApmMiddleware(apmTransaction:apm.Agent){
+    return function(req:Request, res:Response, next:NextFunction){
       const trace = apmTransaction.startTransaction(`${req.method} ${req.url}`, 'request');
       apmAgent.SetTransaction(trace)
       res.on('finish', () => {
@@ -30,10 +34,7 @@ class Router{
         trace.end();
       });
       next();
-    });
-    
-    DataRoutes(router)  
-    return router
+    }
   }
   Run(){
     router = this.Init();
